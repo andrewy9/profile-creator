f5781b31b2e58e2bb7a313e1c26ca7866d9ba56e
andrewy9 <andrew.yan993@gmail.com>
2021-02-22 16:05:38 +1300

    posting server

--- server/db/details.js
+++ server/db/details.js
@@ -1,18 +1,15 @@
 const connection = require('./connection')
 
-function saveDetails ( details, db=connection ) {
+function saveDetails(name, phone, email, profile_intro, db = connection) {
   console.log('hiiiiiiiiiiiiiii')
-  return db('details').insert(details)
+  return db('details')
+    .insert({ name, phone, email, profile_intro })
 }
 
-function getDetails (details, db = connection) {
-  console.log('hiiiiiiiiiiiiiii')
+function getDetails(details, db = connection) {
+  console.log('htting post function?')
   return db('details').select()
 }
-
-
-
-
 
 module.exports = {
   saveDetails,
--- server/routes/detailsRoutes.js
+++ server/routes/detailsRoutes.js
@@ -7,19 +7,18 @@ const router = express.Router()
 router.get('/', (req, res) => {
   console.log('hitting get???')
   db.getDetails()
-  .then(response => {
-    res.json(response)
-  })
+    .then(response => {
+      res.json(response)
+    })
 })
 
 router.post('/', (req, res) => {
   console.log('hitting post?')
-  const details = req.body
-  console.log(details)
-  db.saveDetails(details)
+  const { name, phone, email, profile_intro } = req.body
+  db.saveDetails(name, phone, email, profile_intro)
     .then(details => {
       console.log('details route,', details)
-      res.status(201).res.json(details)
+      res.status(201)
     })
 })
 

9f6d75575f1f36c4135137b9452eaf4d2edee567
sarah-knowles <sjk339@gmail.com>
2021-02-24 14:28:14 +1300

    old employment history test complete

--- server/db/details.test.js
+++ server/db/details.test.js
@@ -2,7 +2,8 @@ const knex = require('knex')
 const config = require('./knexfile')
 const connection = knex(config.test)
 
-const { getDetails, saveDetails, saveEmploymentHistory } = require('./details')
+const { getDetails, saveDetails, saveEmploymentHistory, saveOldEmploymentHistory } = require('./details')
+
 
 beforeAll(() => connection.migrate.latest())
 beforeEach(() => connection.seed.run())
@@ -11,10 +12,10 @@ describe('getDetails', () => {
   test('gets the details', () => {
     expect.assertions(1)
     return getDetails(connection)
-    .then(result => {
-      expect(result.length).toEqual(3)
-      return null
-    })
+      .then(result => {
+        expect(result.length).toEqual(3)
+        return null
+      })
   })
 })
 
@@ -25,22 +26,42 @@ describe('saveDetails', () => {
       .then((newId) => {
         expect(newId).toEqual([4])
         return connection('details').select()
-        .then(result => {
-          expect(result).toHaveLength(4)
-          expect(result[3].name).toEqual('name')
-          return null
-        })
+          .then(result => {
+            expect(result).toHaveLength(4)
+            expect(result[3].name).toEqual('name')
+            return null
+          })
       })
   })
 })
 
 describe('saveEmploymentHistory', () => {
   test('save the employment history to database', () => {
-    expect.assertions(1)
-  return saveEmploymentHistory('employer', 'employmentDate', 'role', 'details', connection)
-    .then((newId) => {
-      expect(newId).toEqual([4])
-      
-    })
+    expect.assertions(3)
+    return saveEmploymentHistory('employer', 'employmentDate', 'role', 'details', connection)
+      .then((newId) => {
+        expect(newId).toEqual([4])
+        return connection('employment_history').select()
+          .then(result => {
+            expect(result).toHaveLength(4)
+            expect(result[3].employer).toEqual('employer')
+          })
+      })
   })
+})
+
+describe('saveOldEmploymentHistory', () => {
+  test('save older employment history to database', () => {
+    expect.assertions(3)
+      return saveOldEmploymentHistory('oldEmployer', 'oldEmploymentDate', 'oldRole', connection)
+        .then((newId) => {
+          expect(newId).toEqual([4])
+          return connection('old_employment_history').select()
+            .then(result => {
+              console.log(result)
+              expect(result).toHaveLength(4)
+              expect(result[3].oldEmployer).toEqual('oldEmployer')
+            })
+        })
+    })
 })

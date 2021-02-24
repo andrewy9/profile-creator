cb2ad4f692d1cd47418acc9ba2d66a70e56423fb
sarah-knowles <sjk339@gmail.com>
2021-02-24 14:08:30 +1300

    saveDetails test complete

--- server/db/details.test.js
+++ server/db/details.test.js
@@ -17,3 +17,20 @@ describe('getDetails', () => {
     })
   })
 })
+
+describe('saveDetails', () => {
+  test('saves the details to database', () => {
+    expect.assertions(3)
+    const test = ['kate', '021', 'test@test.com', 'this is test']
+    return saveDetails(test[0], test[1], test[2], test[3], connection)
+      .then((newId) => {
+        expect(newId).toEqual([4])
+        return connection('details').select()
+        .then(result => {
+          expect(result).toHaveLength(4)
+          expect(result[3].name).toEqual('kate')
+          return null
+        })
+      })
+  })
+})

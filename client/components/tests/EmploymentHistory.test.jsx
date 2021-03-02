4f8edb06bcc2967274b3f387da825837f54c8a1a
Kate-Baya <ksbaya@gmail.com>
2021-03-02 16:07:44 +1300

    working on EmploymentHistory test

---
+++ client/components/tests/EmploymentHistory.test.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import { Provider } from 'react-redux'
+
+import EmploymentHistory from '../EmploymentHistory'
+import { fetchEmploymentHistory } from '../../actions'
+import { Statement } from 'sqlite3'
+
+const fakeStore = {
+  dispatch: jest.fn(),
+  getState: jest.fn(),
+  subscribe: jest.fn()
+}
+
+const fakeAction = { type: 'fakeAction'}
+jest.mock('../../actions', () => ({
+  fetchEmploymentHistory: jest.fn(() => fakeAction)
+}))
+
+describe('<EmploymentHistory />', () => {
+  let input
+  let button
+  describe('input feilds correctly working', () => {
+
+    beforeEach(() => {
+      render(<Provider store={fakeStore}><EmploymentHistory/></Provider>)
+      input = screen.getAllByRole('textbox')
+      input.forEach((el) => {
+        fireEvent.change(el, { target: {value: 'test'}})
+        fireEvent.blur(el)
+      })
+  
+      button = screen.getByRole('button')
+      fireEvent.click(button)
+    })
+    
+    test('submitting an input dispatches fetchEmploymentHistory action', () => {
+      expect(fakeStore.dispatch).toHaveBeenCalledWith(fakeAction)
+      expect(fetchEmploymentHistory).toHaveBeenCalledWith({employmentHistory: [{'employer': 'test', 'employmentDate': 'test', 'role': 'test', 'details': 'test'}]})
+    })
+
+    test('clicking add more button adds more input fields', () => {
+      console.log(button)
+      expect(button).toContain('addEmploymentHistory')
+    })
+  })
+
+  
+})

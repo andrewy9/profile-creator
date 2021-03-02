0068f670e6e16789d51f63beea282984477fa41b
andrewy9 <andrew.yang993@gmail.com>
2021-03-03 11:57:12 +1300

    refactored test

--- client/components/tests/EmploymentHistory.test.jsx
+++ client/components/tests/EmploymentHistory.test.jsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux'
 
 import EmploymentHistory from '../EmploymentHistory'
 import { fetchEmploymentHistory } from '../../actions'
-import { Statement } from 'sqlite3'
 
 const fakeStore = {
   dispatch: jest.fn(),
@@ -19,29 +18,25 @@ jest.mock('../../actions', () => ({
 
 describe('<EmploymentHistory />', () => {
   let input
-  let button
-  describe('input feilds correctly working', () => {
 
-    beforeEach(() => {
-      render(<Provider store={fakeStore}><EmploymentHistory /></Provider>)
-      input = screen.getAllByRole('textbox')
-      input.forEach((el) => {
-        fireEvent.change(el, { target: { value: 'test' } })
-        fireEvent.blur(el)
-      })
+  beforeEach(() => {
+    render(<Provider store={fakeStore}><EmploymentHistory /></Provider>)
+  })
 
-      button = screen.getByRole('button')
-      fireEvent.click(button)
-      input = screen.getAllByRole('textbox')
+  test('submitting an input dispatches fetchEmploymentHistory action', () => {
+    input = screen.getAllByRole('textbox')
+    input.forEach((el) => {
+      fireEvent.change(el, { target: { value: 'test' } })
+      fireEvent.blur(el)
     })
+    expect(fakeStore.dispatch).toHaveBeenCalledWith(fakeAction)
+    expect(fetchEmploymentHistory).toHaveBeenCalledWith({ employmentHistory: [{ 'employer': 'test', 'employmentDate': 'test', 'role': 'test', 'details': 'test' }] })
+  })
 
-    test('submitting an input dispatches fetchEmploymentHistory action', () => {
-      expect(fakeStore.dispatch).toHaveBeenCalledWith(fakeAction)
-      expect(fetchEmploymentHistory).toHaveBeenCalledWith({ employmentHistory: [{ 'employer': 'test', 'employmentDate': 'test', 'role': 'test', 'details': 'test' }] })
-    })
-
-    test('clicking add more button adds more input fields', () => {
-      expect(input).toHaveLength(8)
-    })
+  test('clicking add more button adds more input fields', () => {
+    let button = screen.getByRole('button')
+    fireEvent.click(button)
+    input = screen.getAllByRole('textbox')
+    expect(input).toHaveLength(8)
   })
 })
